acde8d92296151511447ce61c1d16d6db46dc31d
Krrish166 <bijayapatra133@gmail.com>
2025-12-18 12:36:05 +0530

    update

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+
+def main():
+    print("Hello, this is my first Python app!")
+
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+
+    print(f"Hi {name}, you will be {age + 1} years old next year.")
+
+if __name__ == "__main__":
+    main()
+
